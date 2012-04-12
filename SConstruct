a0575ac2a3361f4ee149b175d4a12d6da0c8673e
Chris Tompkinson <tompko@gmail.com>
2012-04-12 21:07:01 +0100

    Testing using the Check framework, basic test for size of bitboard

--- SConstruct
+++ SConstruct
@@ -4,6 +4,9 @@ common_files = []
 main_files = ['main.c']
 test_files=  ['test.c']
 
+test_libs = ['libcheck']
+lib_path = ['/usr/lib', '/usr/local/lib']
+
 VariantDir(variant_dir='obj', src_dir='src', duplicate=False)
 env = Environment()
 
@@ -11,4 +14,4 @@ main_build = ['obj/' + p for p in itertools.chain(common_files, main_files)]
 test_build = ['obj/' + p for p in itertools.chain(common_files, test_files)]
 
 env.Program(target='bin/Patrician', source=main_build)
-env.Program(target='bin/test_suite', source=test_build)
+env.Program(target='bin/test_suite', source=test_build, LIBS=test_libs, LIBPATH=lib_path)
--- src/test.c
+++ src/test.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
 
-int testsPassed = 0;
-int testsFailed = 0;
+#include "board.h"
 
-void main(void)
+START_TEST(test_bitboard_size)
 {
-	int totalTests = testsPassed + testsFailed;
-	printf("Passed %i/%i\n", testsPassed, totalTests);
+	fail_if(sizeof(bitboard) != 8, "Bitboard is not 64 bits, found %i bits instead", sizeof(bitboard)*8);
+}
+END_TEST
+
+Suite* board_suite(void)
+{
+	Suite* s = suite_create("Board");
+
+	TCase* tc_core = tcase_create("Core");
+	tcase_add_test(tc_core, test_bitboard_size);
+	suite_add_tcase(s, tc_core);
+
+	return s;
+}
+
+int main(void)
+{
+	int number_failed;
+
+	Suite* s = board_suite();
+	SRunner* sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;	
 }
