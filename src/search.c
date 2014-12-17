6558cb91ec342e968d4185de3c2e1db4d67b0183
tompko <tompko@gmail.com>
2014-12-17 20:59:18 +0000

    Fix indentation in search.c

--- src/search.c
+++ src/search.c
@@ -9,60 +9,59 @@
 
 int alpha_beta (Board * board, int alpha, int beta, int depth);
 
-Move
-root_search (Board * board)
+Move root_search (Board * board)
 {
-  int i;
-  Move moves[256];
-  Move bestMove;
-  int bestScore = 1000000;
+	int i;
+	Move moves[256];
+	Move bestMove;
+	int bestScore = 1000000;
 
-  unsigned int numMoves = generate_moves (board, moves);
+	unsigned int numMoves = generate_moves (board, moves);
 
-  for (i = 0; i < numMoves; ++i)
+	for (i = 0; i < numMoves; ++i)
 	{
-	  make_move (board, &moves[i]);
-	  int score = alpha_beta (board, -1000000, 1000000, 1);
-	  unmake_move (board, moves[i]);
+		make_move (board, &moves[i]);
+		int score = alpha_beta (board, -1000000, 1000000, 1);
+		unmake_move (board, moves[i]);
 
-	  if (score < bestScore)
-	{
-	  bestScore = score;
-	  bestMove = moves[i];
-	}
+		if (score < bestScore)
+		{
+			bestScore = score;
+			bestMove = moves[i];
+		}
 	}
 
-  return bestMove;
+	return bestMove;
 }
 
-int
-alpha_beta (Board * board, int alpha, int beta, int depth)
+int alpha_beta (Board * board, int alpha, int beta, int depth)
 {
-  int i;
-  Move moves[256];
+	int i;
+	Move moves[256];
 
-  if (depth == 0)
+	if (depth == 0)
 	{
-	  return evaluate (board);
+		return evaluate (board);
 	}
 
-  unsigned int numMoves = generate_moves (board, moves);
+	unsigned int numMoves = generate_moves (board, moves);
 
-  for (i = 0; i < numMoves; ++i)
+	for (i = 0; i < numMoves; ++i)
 	{
-	  make_move (board, &moves[i]);
-	  int score = -alpha_beta (board, -beta, -alpha, depth - 1);
-	  unmake_move (board, moves[i]);
+		make_move (board, &moves[i]);
+		int score = -alpha_beta (board, -beta, -alpha, depth - 1);
+		unmake_move (board, moves[i]);
 
-	  if (score >= beta)
-	{
-	  return beta;		//  fail hard beta-cutoff
-	}
-	  if (score > alpha)
-	{
-	  alpha = score;	// alpha acts like max in MiniMax
-	}
+		if (score >= beta)
+		{
+			return beta;		//  fail hard beta-cutoff
+		}
+		if (score > alpha)
+		{
+			alpha = score;	// alpha acts like max in MiniMax
+		}
 	}
 
-  return alpha;
+	return alpha;
 }
+
